79dba0dc4919672bd711e94d15c0d1a543cf2f34
WONGCH9 <wongch9@oocl.com>
2022-11-30 16:14:41 +0800

    feat: add feature Truck

--- src/main/java/com/oo101/Truck.java
+++ src/main/java/com/oo101/Truck.java
@@ -1,2 +1,16 @@
-package com.oo101;public class Truck {
+package com.oo101;
+
+public class Truck {
+
+    private String name;
+    private int speed;
+
+    public Truck(String name, int speed){
+        this.name = name;
+        this.speed = speed;
+    }
+
+    public String speedUp(){
+        return name + "speed up"+ speed + "km/h";
+    }
 }
