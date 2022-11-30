aefa0c8d3aa7c5989947fbdfd5ee92ae74441d56
WONGCH9 <wongch9@oocl.com>
2022-11-30 16:43:11 +0800

    Refactor: code refactor

--- src/main/java/com/oo101/Driver.java
+++ src/main/java/com/oo101/Driver.java
@@ -4,21 +4,21 @@ public class Driver {
 
     private Vehical vehical;
 
-    public Driver(Vehical vehical){
+    public Driver(Vehical vehical) {
         this.vehical = vehical;
     }
 
-    public String speedUp(){
+    public String speedUp() {
         return vehical.speedUp();
     }
 
 
     public static void main(String[] args) {
 
-         Driver driver = new Driver(new Car("Cool Car", new GasolineEngine()));
-        System.out.println(         driver.speedUp() );
-
-
+        Driver driver = new Driver(new Car("Cool Car", new GasolineEngine()));
+        System.out.println(driver.speedUp());
+        driver = new Driver(new Truck("Big Truck", 10));
+        System.out.println(driver.speedUp());
 
     }
 
